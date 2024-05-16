e4486580e9c11a073b0a61ed000d01e2228d3c64
“MikhelCB” <srrynow@hotmail.com>
2024-05-16 08:01:57 -0300

    request and response is made to obtain message ID and USER

--- src/server.js
+++ src/server.js
@@ -2,5 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.get("/message/:id/:user", (resquest, response) => {
+  const { id, user } = resquest.params;
+
+  response.send(`
+    Mensagem ID: ${id}. 
+    Para o usuário: ${user}.
+    `);
+});
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
